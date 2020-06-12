3004a654fc24b069d892597e55dd206183fa625c
Stephen <stephen.ryan87.sr@gmail.com>
2020-06-12 13:25:05 -0400

    testing git

--- src/main/java/com/shoppingcart/ShoppingCart/ShoppingCartApplication.java
+++ src/main/java/com/shoppingcart/ShoppingCart/ShoppingCartApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoppingCartApplication {
 
+	//App starting point
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartApplication.class, args);
 	}
