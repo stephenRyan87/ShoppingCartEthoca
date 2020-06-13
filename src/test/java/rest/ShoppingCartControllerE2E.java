f2a2e62216f76b31731d97929363f4a6cc4d51e5
Stephen <stephen.ryan87.sr@gmail.com>
2020-06-13 17:40:37 -0400

    Added E2E test for Shopping cart controller to verify functionality

--- src/test/java/rest/ShoppingCartControllerE2E.java
+++ src/test/java/rest/ShoppingCartControllerE2E.java
@@ -1,5 +1,6 @@
 package rest;
 
+import com.shoppingcart.ShoppingCart.ShoppingCartApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ShoppingCartApplication.class)
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
 public class ShoppingCartControllerE2E {
 
@@ -23,12 +27,26 @@ public class ShoppingCartControllerE2E {
     private MockMvc mockMvc;
 
     @Test
-    public void shouldCreatePerson() throws Exception {
-        mockMvc.perform(post("/person")
+    public void shouldCreateOrder() throws Exception {
+        mockMvc.perform(post("/cart/orders")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content("{\"firstName\":\"John\",\"lastName\":\"Doe\",\"age\":30}")
+                .content("{\"orderProducts\": [ " +
+                        "{\"name\":\"spoon\",\"price\":10.0,\"quantity\":3}, " +
+                        "{\"name\":\"fridge\",\"price\":1000.0,\"quantity\":1}]}")
                 .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(content().string(""));
+    }
+
+    @Test
+    public void shouldGetProducts() throws Exception
+    {
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                .get("/cart/products")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.productList").exists());
     }
 }
