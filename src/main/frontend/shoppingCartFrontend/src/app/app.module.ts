e89599a1dee56d993690dae6ac2f9af64b29d23d
Stephen <stephen.ryan87.sr@gmail.com>
2020-06-14 14:08:52 -0400

    Created child components products, orders and shopping cart

--- src/main/frontend/shoppingCartFrontend/src/app/app.module.ts
+++ src/main/frontend/shoppingCartFrontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreFrontComponent } from './store-front/store-front.component';
+import { ProductsComponent } from './storeFront/products/products.component';
+import { OrdersComponent } from './store-front/orders/orders.component';
+import { ShoppingCartComponent } from './store-front/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StoreFrontComponent
+    StoreFrontComponent,
+    ProductsComponent,
+    OrdersComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
