7e0570139885207bceeb569eec99d982892578de
Stephen <stephen.ryan87.sr@gmail.com>
2020-06-14 15:56:38 -0400

    Added service to talk to backend and proxy for url

--- src/main/frontend/shoppingCartFrontend/src/app/app.module.ts
+++ src/main/frontend/shoppingCartFrontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreFrontComponent } from './store-front/store-front.component';
-import { ProductsComponent } from './storeFront/products/products.component';
+import { ProductsComponent } from './store-front/products/products.component';
 import { OrdersComponent } from './store-front/orders/orders.component';
 import { ShoppingCartComponent } from './store-front/shopping-cart/shopping-cart.component';
 
--- src/main/frontend/shoppingCartFrontend/src/app/store-front/store-front.component.ts
+++ src/main/frontend/shoppingCartFrontend/src/app/store-front/store-front.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ProductsComponent} from "./products/products.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
+import {OrdersComponent} from "./orders/orders.component";
 
 @Component({
   selector: 'app-store-front',
@@ -10,6 +13,33 @@ export class StoreFrontComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  private collapsed = true;
+  orderFinished = false;
+
+  @ViewChild('productsC')
+  productsC: ProductsComponent;
+
+  @ViewChild('shoppingCartC')
+  shoppingCartC: ShoppingCartComponent;
+
+  @ViewChild('ordersC')
+  ordersC: OrdersComponent;
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+  finishOrder(orderFinished: boolean) {
+    this.orderFinished = orderFinished;
+  }
+
+  reset() {
+    this.orderFinished = false;
+    this.productsC.reset();
+    this.shoppingCartC.reset();
+    this.ordersC.paid = false;
   }
 
 }
