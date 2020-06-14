22fa8d3379ee3ac97ff910d49973fdc211c8ed4f
Stephen <stephen.ryan87.sr@gmail.com>
2020-06-14 13:53:57 -0400

    Created base component storeFront

--- src/main/frontend/shoppingCartFrontend/src/app/app.module.ts
+++ src/main/frontend/shoppingCartFrontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreFrontComponent } from './store-front/store-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreFrontComponent
   ],
   imports: [
     BrowserModule,
